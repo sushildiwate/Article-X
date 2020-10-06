fd9a058b0d7fc6b462bd250268a68606b34fa165
Sushil Diwate <sushil.diwate@leapscale.com>
2020-10-06 16:12:16 +0530

    Initial Commit

---
+++ app/src/main/java/com/sushil/articlex/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sushil.articlex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
